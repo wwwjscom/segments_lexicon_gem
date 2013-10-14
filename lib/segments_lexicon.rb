93ca1f9e8a9b1fbb1a54abc53bb7b343eb765aea
Jason Soo <wwwjscom@gmail.com>
2013-10-13 21:29:34 -0400

    Fixing files

--- lib/segments_lexicon.rb
+++ lib/segments_lexicon.rb
@@ -1,6 +1,18 @@
+require 'sqlite3'
 require "segments_lexicon/version"
-require "segments_lexicon/lexicon"
 
 module SegmentsLexicon
-  # Your code goes here...
+  # Opens a connection to the db
+  #
+  # param sqlite_db_path [String] Path to the sqlite3 db file
+  def initialize(sqlite_db_path, table_name)
+    @db = SQLite3::Database.new(sqlite_db_path)
+    @db.results_as_hash = true
+    @table = table_name
+    self
+  end
+
+  def search(query_term)
+    results = @db.execute "select * from #{@table} WHERE word LIKE \"#{query_term.downcase}\" COLLATE NOCASE"
+  end
 end
--- lib/segments_lexicon/version.rb
+++ lib/segments_lexicon/version.rb
@@ -1,3 +1,3 @@
-module SegmentsLexicon
+class SegmentsLexicon
   VERSION = "0.0.1"
 end
